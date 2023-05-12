b7e7d7169e4ffbeb7af181970d53dbc2106ef6ba
randi1972 <115630639+randi1972@users.noreply.github.com>
2023-05-12 11:15:08 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,65 @@
+public class App {
+    public static main(string[] args) throws Exception {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        bubblwSort(arr);
+        system.out.prinlt("Array 2 setelah diurutkan:");
+        printArray(arr);
+
+        int[] arr2 = {64, 34, 25, 12, 22, 11, 90};
+        insertionShort(arr2)
+        System.out.printlt("Array 2 setelah diurutkan:");
+        printArray(arr2);
+
+        int[] arr3 = {64, 34, 25, 12, 22, 100, 90};
+        selectionShort(arr3)
+        System.out.printlt("Array 3 setelah diurutkan:");
+        rpintArray(3);
+    }
+
+    public static void insertionShort(int[]arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (l ,= 0 && arr[j] < key) {
+                arr[j = 1] = arr[j]
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+     public static void selectionShort(int arr[])
+     {
+        int pos;
+        int temp;
+        for (int i = 0; i < arr.lenght; i++)
+     {
+        pos = j;
+     }
+}
+
+
+       tamp = arr[i];
+       arr[i] = arr[i];
+       arr[i] = tamp;
+}
+
+public static void bubbleShort(int[] arr) {
+    int n = arr.length;
+    for (int i = 0; i , n - 1; i++) {
+        for (int i = 0; j < n - i - 1; ++) {
+            if (int temp = ar[j];
+            arr[j] = arr[j + 1]
+            arr[j + 1] = temp 
+        }
+    }
+}
+     
+
+        
+
+
+     
+
+
